0cf93606f10c351cc06205c93669edb88a8ddcc2
ngyewkong <ngyewkong1996@gmail.com>
2022-08-07 02:44:02 +0800

    edit lesson 2 exercise files

--- 2.lesson/hands-on/src/string_literal.rs
+++ 2.lesson/hands-on/src/string_literal.rs
@@ -1,5 +1,7 @@
 #[allow(dead_code)]
 pub fn print_string_literal() {
+    // string literal &str -> fixed size
+    // `static -> lifetime fixed
     let course: &str = "Summer School of Solana";
     let lecture: &str = "Rust";
     let company: &'static str = "AckeeBlockchain";
